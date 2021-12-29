754370706e3c5093450ead6a4cdcdd67554351cf
Mhamad Ibrahim (σκιά ツ) <Skiastep@gmail.com>
2021-12-29 13:39:14 +0200

    implement externalApi related route

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { NavBar, Footer, Loading } from "./components";
-import { Home, Profile } from "./views";
+import { Home, Profile, ExternalApi } from "./views";
 import ProtectedRoute from "./auth/protected-route";
 
 import "./app.css";
@@ -22,6 +22,7 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Home} />
           <ProtectedRoute path="/profile" component={Profile} />
+          <ProtectedRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </div>
       <Footer />
